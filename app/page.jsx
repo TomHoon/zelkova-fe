42f35c0b5b0b6b50c82dc7c68daf942821f8b9c2
tomhoon <gnsdl9108@naver.com>
2025-05-25 12:23:50 +0900

    feat: designSystem 구축을 위한 셋팅

---
+++ app/page.jsx
@@ -0,0 +1,6 @@
+import C_Button from "@/common/atom/C_Button";
+import C_TabBar from "@/common/mocules/C_TabBar";
+
+export default function App() {
+  return <div>hello</div>;
+}
---
+++ common/mocules/C_TabBar.jsx
@@ -0,0 +1,43 @@
+"use client";
+
+import TabBarStyles from "@/styles/C_TabBar.module.scss";
+import { useState } from "react";
+
+/**
+ *
+ * [공통탭바 - Args]
+ *
+ * - 탭내용리스트: 개수에 따라 너비 결정
+ * - 콜백: 탭 클릭시 발동할 함수(없을 경우 동작없음)
+ *
+ *  */
+
+export default function TabBar({ elementList = [], callback }) {
+  const [activeIdx, setActiveIdx] = useState(0);
+
+  const clickedThroughProxy = (idx) => {
+    setActiveIdx(idx);
+
+    if (typeof callback == "function") {
+      callback();
+    }
+  };
+
+  return (
+    <>
+      <div className={TabBarStyles.container}>
+        <ul>
+          {elementList.map((item, idx) => (
+            <li
+              key={idx}
+              onClick={() => clickedThroughProxy(idx)}
+              className={idx == activeIdx ? TabBarStyles.active : ""}
+            >
+              <span>{item}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+}
