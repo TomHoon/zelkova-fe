cefa9c67cd4c3d24492dfc657457f9157a58e696
dnjswns5013 <dnjswns5013@naver.com>
2025-05-30 00:15:42 +0900

    C_CommentItem 추가 C_Button small 사이즈 추가

--- common/atom/C_Button.jsx
+++ common/atom/C_Button.jsx
@@ -5,6 +5,7 @@ import C_ButtonStyles from "@/styles/C_Button.module.scss";
 const SizeMap = {
   attach: C_ButtonStyles.attach,
   nav: C_ButtonStyles.nav,
+  small: C_ButtonStyles.small,
   medium: C_ButtonStyles.medium,
   wide: C_ButtonStyles.wide,
   large: C_ButtonStyles.large,
@@ -20,7 +21,7 @@ const ColorTypeMap = {
 /**
  * [공통버튼 - Args]
  *
- * - 크기: attach, nav, medium, wide, large, xlarge
+ * - 크기: attach, nav, small, medium, wide, large, xlarge
  * - 컬러: A (white 배경 + green 테두리), B (green 배경 + white 텍스트), C (white 배경 + green 테두리 + green 텍스트)
  * - 글자: title (기본값 "버튼")
  * 
